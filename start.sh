c998ed3a960263d413075df83e58f99ff447b399
tor-on-synology <tor-on-synology@users.noreply.github.com>
2020-12-28 01:08:59 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 chmod a+w /dev/pts/0
-exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
-exec /usr/bin/tor -f /etc/tor/torrc
+lighttpd -D -f /etc/lighttpd/lighttpd.conf
+/usr/bin/tor -f /etc/tor/torrc
